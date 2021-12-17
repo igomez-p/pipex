c5b64e01510b544a4ac2a8fcd3861bd16f251263
igomez-p <ire.go.pla@gmail.com>
2021-12-17 17:43:16 +0100

    unset path ok

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: igomez-p <ire.go.pla@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:37:24 by igomez-p          #+#    #+#             */
-/*   Updated: 2021/12/17 16:53:17 by igomez-p         ###   ########.fr       */
+/*   Updated: 2021/12/17 17:42:50 by igomez-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,22 @@ void	read_stack(t_data *d, int argc, char **argv, char **envp)
 		clean_exit(d, FAIL);
 	else
 	{
-		while (!ft_strnstr(*envp, PATH, 4))
+		while (*envp && !ft_strnstr(*envp, PATH, 4))
 			envp++;
-		paths = ft_split(*envp + 5, ':');
-		d->c1 = ft_split(argv[2], ' ');
-		d->c2 = ft_split(argv[3], ' ');
-		check_command(d, d->c1[0], d->c2[0], paths);
-		free_double(paths);
-		if (!d->path1 || !d->path2 || !check_files(d, argv[1], argv[4]))
+		if (*envp)
+		{
+			paths = ft_split(*envp + 5, ':');
+			d->c1 = ft_split(argv[2], ' ');
+			d->c2 = ft_split(argv[3], ' ');
+			check_command(d, d->c1[0], d->c2[0], paths);
+			free_double(paths);
+			if (!d->path1 || !d->path2 || !check_files(d, argv[1], argv[4]))
+				clean_exit(d, FAIL);
+			d->cmd1 = argv[2];
+			d->cmd2 = argv[3];
+		}
+		else
 			clean_exit(d, FAIL);
-		d->cmd1 = argv[2];
-		d->cmd2 = argv[3];
 	}
 }
 
@@ -38,7 +43,7 @@ static void	child_process(t_data *d, int *fd, char **envp)
 {
 	int	fd1;
 
-	fd1 = open(d->file1, O_RDONLY, 0777);
+	fd1 = open(d->file1, O_RDONLY, 0644);
 	if (fd1 == -1)
 		clean_exit(d, FAIL);
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
@@ -56,7 +61,7 @@ static void	parent_process(t_data *d, int *fd, char **envp)
 {
 	int	fd2;
 
-	fd2 = open(d->file2, O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	fd2 = open(d->file2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd2 == -1)
 		clean_exit(d, FAIL);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
